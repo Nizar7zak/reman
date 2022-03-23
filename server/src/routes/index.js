02dea048e572834c78fb1e4c37e69098af02cd37
nizar7zak <nizar7zakout@gmail.com>
2022-03-24 00:45:56 +0200

    handle server error

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
 const asyncMiddleWare = require('../middlewares/async');
+const serverError = require('../middlewares/serverError');
+
 const login = require('./login');
 const signUp = require('./signup');
 
 router.post('/login', asyncMiddleWare(login));
 router.post('/signup', asyncMiddleWare(signUp));
+
+router.use(serverError);
 
 module.exports = router;
