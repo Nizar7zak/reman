3860525e5f68bfc2c1c460370a960d0926a57ed8
nizar7zak <nizar7zakout@gmail.com>
2022-03-23 21:05:10 +0200

    check user exist in database before compare password

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -6,6 +6,9 @@ const login = async (req, res) => {
 
   const { error } = await loginSchema.validateAsync(req.body);
   if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const { rows } = await getUserByEmail(email);
+  if (!rows.length) return res.status(400).send({ message: 'Invalid Email or Password' });
 };
 
 module.exports = login;
