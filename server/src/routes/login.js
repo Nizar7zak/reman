42b7a7e3e583a3f8fc865036aed41a4dec23ea10
nizar7zak <nizar7zakout@gmail.com>
2022-03-24 00:06:40 +0200

    set cookie for a logged user

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,4 +1,5 @@
 const { compare } = require('bcrypt');
+const { sign } = require('jsonwebtoken');
 
 const { getUserByEmail } = require('../database/queries/user');
 const { loginSchema } = require('../utils');
@@ -8,14 +9,23 @@ const login = async (req, res) => {
   if (error) return res.status(400).send({ message: error.details[0].message });
 
   const { rows } = await getUserByEmail(req.body.email);
-  if (!rows.length) return res.status(400).send({ message: 'Invalid Email or Password' });
+  if (!rows.length) { return res.status(400).send({ message: 'Invalid Email or Password' }); }
 
   const {
     id, name, email, password, isadmin,
   } = rows[0];
 
   const vaildPassword = await compare(req.body.password, password);
-  if (!vaildPassword) return res.status(400).send({ message: 'Invalid Email or Password' });
+  if (!vaildPassword) { return res.status(400).send({ message: 'Invalid Email or Password' }); }
+
+  const token = sign({ id, email, isadmin }, process.env.SECRETTOKENKEY);
+
+  res.status(201).cookie('token', token).send({
+    message: 'You are Logged Successfully',
+    data: {
+      id, name, isadmin, email,
+    },
+  });
 };
 
 module.exports = login;
