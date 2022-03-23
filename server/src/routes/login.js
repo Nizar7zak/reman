ccf5babfd1252f4b5ac4159120feddf858e562cd
nizar7zak <nizar7zakout@gmail.com>
2022-03-24 00:39:47 +0200

    handle validation error response - try catch

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -5,8 +5,11 @@ const { getUserByEmail } = require('../database/queries/user');
 const { loginSchema } = require('../utils');
 
 const login = async (req, res) => {
-  const { error } = await loginSchema.validateAsync(req.body);
-  if (error) return res.status(400).send({ message: error.details[0].message });
+  try {
+    await loginSchema.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).send({ message: error.details[0].message });
+  }
 
   const { rows } = await getUserByEmail(req.body.email);
   if (!rows.length) { return res.status(400).send({ message: 'Invalid Email or Password' }); }
