064356410a645464b1d3480e6c31250e2d45dbc9
nizar7zak <nizar7zakout@gmail.com>
2022-03-23 21:13:30 +0200

    compare password with hashed password

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,14 +1,21 @@
+const { compare } = require('bcrypt');
+
 const { getUserByEmail } = require('../database/queries/user');
 const { loginSchema } = require('../utils');
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-
   const { error } = await loginSchema.validateAsync(req.body);
   if (error) return res.status(400).send({ message: error.details[0].message });
 
-  const { rows } = await getUserByEmail(email);
+  const { rows } = await getUserByEmail(req.body.email);
   if (!rows.length) return res.status(400).send({ message: 'Invalid Email or Password' });
+
+  const {
+    id, name, email, password, isadmin,
+  } = rows[0];
+
+  const vaildPassword = await compare(req.body.password, password);
+  if (!vaildPassword) return res.status(400).send({ message: 'Invalid Email or Password' });
 };
 
 module.exports = login;
