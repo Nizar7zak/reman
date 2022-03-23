032baca59bad76f9570a15312668d4e533200647
nizar7zak <nizar7zakout@gmail.com>
2022-03-24 00:41:50 +0200

    handle validation error sign up  response - try catch

--- server/src/routes/signup.js
+++ server/src/routes/signup.js
@@ -5,9 +5,11 @@ const { signUpSchema } = require('../utils');
 
 const signUp = async (req, res) => {
   const { email, password, name } = req.body;
-
-  const { error } = await signUpSchema.validateAsync(req.body);
-  if (error) return res.status(400).send({ message: error.details[0].message });
+  try {
+    await signUpSchema.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).send({ message: error.details[0].message });
+  }
 
   const { rowCount } = await getUserByEmail(email);
   if (rowCount) return res.status(400).send({ message: 'User already registered.' });
