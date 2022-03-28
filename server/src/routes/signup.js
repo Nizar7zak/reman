3a56e639e1121cfa14f88c0cea7be3395ae928bb
nizar7zak <nizar7zakout@gmail.com>
2022-03-28 10:46:05 +0300

    handle validation error - catching in async middleware

--- server/src/routes/signup.js
+++ server/src/routes/signup.js
@@ -5,20 +5,18 @@ const { signUpSchema } = require('../utils');
 
 const signUp = async (req, res) => {
   const { email, password, name } = req.body;
-  try {
-    await signUpSchema.validateAsync(req.body);
-  } catch (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
+  await signUpSchema.validateAsync(req.body);
 
   const { rowCount } = await getUserByEmail(email);
-  if (rowCount) return res.status(400).json({ message: 'User already registered.' });
+  if (rowCount) { return res.status(400).json({ message: 'User already registered.' }); }
 
   const salt = await genSalt(10);
   const hashed = await hash(password, salt);
 
   const result = await insertNewUser({ name, email, password: hashed });
-  return res.status(201).json({ message: 'registered successfully', data: result.rows[0] });
+  return res
+    .status(201)
+    .json({ message: 'registered successfully', data: result.rows[0] });
 };
 
 module.exports = signUp;
