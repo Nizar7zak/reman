84c90029fbc50e39059767e0dbbefaa05b010264
nizar7zak <nizar7zakout@gmail.com>
2022-04-11 03:18:41 +0300

    edit any column in product base on name

--- server/src/controllers/products/editProduct.js
+++ server/src/controllers/products/editProduct.js
@@ -2,11 +2,18 @@ const { editProductQuery, getProductByName } = require('../../database/queries/p
 const { uploadToCloudinary } = require('../../utils');
 
 const editProduct = async (req, res) => {
-  const { name, image } = req.body;
+  const {
+    name, image, price, category, discount,
+  } = req.body;
+
   const { rowCount } = await getProductByName(name);
   if (!rowCount) return res.status(404).json({ message: 'there is no Product with this name' });
+
   const { url } = await uploadToCloudinary(image, { upload_preset: 'dev_setup' });
-  const { rows } = await editProductQuery({ image: url, name });
+
+  const { rows } = await editProductQuery({
+    image: url, name, price, category, discount,
+  });
   return res.status(202).json({ data: rows[0], message: 'product updated' });
 };
 
--- server/src/database/queries/products/editProductQuery.js
+++ server/src/database/queries/products/editProductQuery.js
@@ -1,8 +1,13 @@
 const connection = require('../../config/connection');
 
-const editProductQuery = ({ image, name }) => connection.query('UPDATE products SET image = $1 WHERE name = $2 RETURNING *', [
+const editProductQuery = ({
+  image, name, price, category, discount,
+}) => connection.query('UPDATE products SET image = $1, price = $3, category_id = $4, discount = $5 WHERE name = $2 RETURNING *', [
   image,
   name,
+  price,
+  category,
+  discount,
 ]);
 
 module.exports = editProductQuery;
